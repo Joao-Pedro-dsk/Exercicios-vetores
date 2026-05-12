66e4aeb241d964761d1d4562c9a9b4c8a5c40bea
Joao-Pedro-dsk <jpengregoso@hotmail.com>
2026-05-12 20:29:06 -0300

    Add files via upload

---
+++ E1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    int V[10];
+    int n, n2;
+
+    for(n=0; n<10; n++){
+        scanf("%i", &V[n]);
+    }
+    for(n2=0; n2<10; n2++){
+    printf("%i\n", V[n2]);
+    }
+}
